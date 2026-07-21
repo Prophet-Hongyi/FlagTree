249b8861aacb18997523bc9f9ce1e307f5f807c1
lzllx123 <1803100521@qq.com>
2026-07-21 20:26:09 +0800

    [TLE]fix(iluvatar):adapt TLE extract_tile,insert_tile ops to strides API

--- third_party/iluvatar/tle/triton_iluvatar_tle.cc
+++ third_party/iluvatar/tle/triton_iluvatar_tle.cc
@@ -101,18 +101,24 @@ void init_triton_iluvatar_tle_ir(py::module m) {
            })
       .def("create_extract_tile",
            [](TritonOpBuilder &self, mlir::Value &input, mlir::Value &index,
-              std::vector<int64_t> &tileShape) -> mlir::Value {
+              std::vector<int64_t> &tileShape,
+              std::vector<int64_t> strides) -> mlir::Value {
              auto op = self.create<iluvatar_tle::ExtractTileOp>(input, index,
-                                                                tileShape);
+                                                                tileShape,
+                                                                strides);
              return op.getResult();
-           })
+           },
+           py::arg("input"), py::arg("index"), py::arg("tileShape"),
+           py::arg("strides") = std::vector<int64_t>{})
       .def("create_insert_tile",
            [](TritonOpBuilder &self, mlir::Value &input, mlir::Value &tile,
-              mlir::Value &index) -> mlir::Value {
-             auto op =
-                 self.create<iluvatar_tle::InsertTileOp>(input, tile, index);
+              mlir::Value &index, std::vector<int64_t> strides) -> mlir::Value {
+             auto op = self.create<iluvatar_tle::InsertTileOp>(input, tile, index,
+                                                               strides);
              return op.getResult();
-           })
+           },
+           py::arg("input"), py::arg("tile"), py::arg("index"),
+           py::arg("strides") = std::vector<int64_t>{})
       .def("create_local_pointers",
            [](TritonOpBuilder &self, mlir::Type resultTy, mlir::Value memDesc,
               py::args args) -> mlir::OpState {
