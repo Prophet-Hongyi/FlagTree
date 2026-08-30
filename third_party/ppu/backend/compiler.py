3d52679639e7ba978e68df05c1d20054cd12618b
Prophet <316497798+Prophet-Hongyi@users.noreply.github.com>
2026-08-30 09:34:18 +0800

    [PPU] Declare batched dot_scaled support

--- third_party/ppu/backend/compiler.py
+++ third_party/ppu/backend/compiler.py
@@ -163,6 +163,7 @@ class HGGCOptions:
     launch_pdl: bool = False
     supported_fp8_dtypes: Tuple[str] = ("fp8e5", "fp8e4b15")
     deprecated_fp8_dot_operand_dtypes: Tuple[str] = ()
+    supports_batched_dot_scaled: bool = True
     default_dot_input_precision: str = "tf32"
     allowed_dot_input_precisions: Tuple[str] = ("tf32", "tf32x3", "ieee", 'bf16x3', 'bf16x6')
     max_num_imprecise_acc_default: bool = None
